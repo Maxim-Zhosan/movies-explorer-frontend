f32a596f9ff9cd5aa838a8faeb021d7f09d9819d
Maxim Zhosan <maxim.zhosan@gmail.com>
2023-02-19 20:24:42 +0100

    bugfix: update user info

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,11 +3,12 @@
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import LoggedInContext from '../contexts/LoggedInContext';
+import Preloader from './Preloader/Preloader';
 
 function ProtectedRoute({ isLoading, element }) {
   const isLoggedIn = useContext(LoggedInContext);
   return (
-    isLoggedIn ? element : (isLoading ? element : <Navigate to="/" />)
+    isLoggedIn ? element : (isLoading ? <Preloader /> : <Navigate to="/" />)
   );
 }
 
